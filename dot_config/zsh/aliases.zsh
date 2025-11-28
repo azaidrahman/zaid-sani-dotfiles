547094de6a8e635848e32254f59218aed9abf03e
Zaid <azaidrahman@gmail.com>
2025-11-28 14:30:58 +0800

    zsh: add all new aliases

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -9,7 +9,25 @@ alias czm="tree -L 2 -- $(chezmoi source-path)" # Simpler chezmoi managed
 alias uvoprun='op run --env-file=.env -- uv run main.py'
 
 alias oc="opencode"
+alias c="claude"
+alias cres="claude --resume" 
 
 alias cl="gcloud"
 
 alias st="speedtest -A"
+alias tm="tmux attach -t main"
+alias tma="tmux attach -t"
+
+alias ld="eza -lD"
+alias lf="eza -lf --color=always | grep -v /"
+alias lh="eza -dl .* --group-directories-first"
+alias ll="eza -al --group-directories-first"
+# alias ls="eza -alf --color=always --sort=size | grep -v /"
+alias ls="eza -al --color=always --sort=size"
+alias lt="eza -al --sort=modified"
+
+alias setalias="nvim ~/.config/zsh/aliases.zsh"
+
+alias tf="terraform"
+
+alias cat="bat"
