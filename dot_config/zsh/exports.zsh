3eea36697db24a6b33e011b795fee0bca1ac1320
Zaid <azaidrahman@gmail.com>
2025-12-22 21:20:33 +0800

    zsh: add exports

--- dot_config/zsh/exports.zsh
+++ dot_config/zsh/exports.zsh
@@ -3,6 +3,7 @@ export PATH="/opt/homebrew/bin:$PATH"
 export XDG_CONFIG_HOME="$HOME/.config"
 
 export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$HOME/.local/bin:$PATH"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init - zsh)"
 
